6f8f289405466296e36da63c5788b6efc589c16c
HaydarYigitAktas <106775118+HaydarYigitAktas@users.noreply.github.com>
2022-06-11 16:23:41 +0300

    C# .net PAC-MAN
    C# .net çatısı altında yapılmış PAC-MAN oyunu.

---
+++ PAC-MAN V2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace PAC_MAN_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.KırmızıHayalet = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 587);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(598, 85);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // KırmızıHayalet
+            // 
+            this.KırmızıHayalet.Interval = 200;
+            this.KırmızıHayalet.Tick += new System.EventHandler(this.KırmızıHayalet_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::PAC_MAN_V2.Properties.Resources.haritaa;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 85);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(598, 610);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::PAC_MAN_V2.Properties.Resources.kırmızı_hayalet;
+            this.pictureBox2.Location = new System.Drawing.Point(199, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::PAC_MAN_V2.Properties.Resources.Başlıksız_2;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PUAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(598, 695);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer KırmızıHayalet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PAC-MAN V2/Form1.cs
@@ -0,0 +1,542 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+
+namespace PAC_MAN_V2
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[,] yemler = new PictureBox[29, 26];
+        Random rasgele = new Random();
+        int YenenYem = 0;
+        Byte hak = 3;
+
+        byte yön;
+        byte İstenenYön;
+
+        int karakterKonumuX = 14;
+        int karakterKonumuY = 0;
+
+
+        byte H_yön = 1;
+        byte H_İstenenYön = 1;
+        int KırmızıHayaletX = 14;
+        int KırmızıHayaletY = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //---------------------------------- yemmmmmm ---------------------------------------------
+        void YemYerleştir()
+        {
+            int yatay = 21;
+            int dikey = 21;
+
+            for (int Satır = 0; Satır < 29; Satır++)
+            {
+                for (int sütun = 0; sütun < 26; sütun++)
+                {
+
+                    yemler[Satır, sütun] = new PictureBox();
+                    yemler[Satır, sütun].Name = Convert.ToString(Satır + "-" + sütun);
+                    yemler[Satır, sütun].Size = new Size(5, 5);
+                    yemler[Satır, sütun].Location = new Point(yatay, dikey);
+                    yemler[Satır, sütun].BackColor = Color.Yellow;
+
+                    yatay += 16;
+
+                    panel2.Controls.Add(yemler[Satır, sütun]);
+                }
+                yatay = 21;
+                dikey += 16;
+            }
+        }
+
+        void YemSil(byte başlangıçNoktasıY, byte başlangıçNoktasıX, byte uzunlukY, byte uzunlukX)
+        {
+
+            int yatay = başlangıçNoktasıY;
+            int dikey = başlangıçNoktasıX;
+
+            for (int Satır = yatay; Satır < yatay + uzunlukY; Satır++)
+            {
+                for (int sütun = dikey; sütun < dikey + uzunlukX; sütun++)
+                {
+                    yemler[Satır, sütun].BackColor = Color.Transparent;
+
+                }
+            }
+        }
+
+        void BütünYemleriSil()
+        {
+            //Ymax = 29
+            //Xmax = 25
+            YemSil(1, 1, 3, 4);
+            YemSil(1, 6, 3, 5);
+            YemSil(0, 12, 4, 2);
+            YemSil(1, 15, 3, 5);
+            YemSil(1, 21, 3, 4);
+            YemSil(5, 1, 2, 4);
+            YemSil(8, 0, 5, 5);
+            YemSil(14, 0, 5, 5);
+            YemSil(23, 0, 2, 2);
+            YemSil(5, 9, 2, 8);
+            YemSil(5, 21, 2, 4);
+            YemSil(5, 6, 8, 2);
+            YemSil(6, 12, 4, 2);
+            YemSil(5, 18, 8, 2);
+            YemSil(8, 21, 5, 5);
+            YemSil(14, 21, 5, 5);
+            YemSil(8, 8, 2, 3);
+            YemSil(8, 15, 2, 3);
+            YemSil(11, 9, 5, 8);
+            YemSil(26, 1, 2, 10);
+            YemSil(26, 15, 2, 10);
+            YemSil(23, 6, 3, 2);
+            YemSil(23, 9, 2, 8);
+            YemSil(23, 12, 5, 2);
+            YemSil(23, 18, 5, 2);
+            YemSil(20, 1, 2, 4);
+            YemSil(20, 6, 2, 5);
+            YemSil(20, 15, 2, 5);
+            YemSil(20, 21, 2, 4);
+            YemSil(22, 3, 3, 2);
+            YemSil(22, 21, 3, 2);
+            YemSil(23, 24, 2, 2);
+            YemSil(14, 6, 5, 2);
+            YemSil(14, 18, 5, 2);
+            YemSil(17, 9, 2, 8);
+            YemSil(19, 12, 3, 2);
+
+        }
+
+        //---------------------------------- yemmmmmm ---------------------------------------------
+
+        //---------------------------------- karakter ---------------------------------------------
+
+        Point PAC_MAN(int Y, int X)
+        {
+            int y = yemler[Y, X].Location.Y;
+            int x = yemler[Y, X].Location.X;
+
+            Point nokta = new Point(x - 7, y - 7);
+
+            return nokta;
+        }
+
+        void İstenenYönKontrol(int X, int Y, byte istenenYön, byte kim) // Karakter konumu girilecek
+        {
+            // kim değişkeninde 1 pac_man, 2Kırmızı hayalet
+
+            int x = X;
+            int y = Y;
+
+            byte istenenYönn = istenenYön;
+
+            if (istenenYönn == 1)
+            {
+                if (x++ < 25 && yemler[y, x].BackColor != Color.Transparent)
+                {
+                    if (kim == 1)
+                    {
+                        yön = 1;
+                    }
+                    if (kim == 2)
+                    {
+                        H_yön = 1;
+                    }
+                }
+            }
+            else if (istenenYönn == 2)
+            {
+
+                if (x-- > 0 && yemler[y, x].BackColor != Color.Transparent)
+                {
+                    if (kim == 1)
+                    {
+                        yön = 2;
+                    }
+                    if (kim == 2)
+                    {
+                        H_yön = 2;
+                    }
+                }
+            }
+            else if (istenenYönn == 4)
+            {
+                if (y++ < 28 && yemler[y, x].BackColor != Color.Transparent)
+                {
+                    if (kim == 1)
+                    {
+                        yön = 4;
+                    }
+                    if (kim == 2)
+                    {
+                        H_yön = 4;
+                    }
+
+                }
+            }
+            else if (istenenYönn == 3)
+            {
+                if (y-- > 0 && yemler[y, x].BackColor != Color.Transparent)
+                {
+                    if (kim == 1)
+                    {
+                        yön = 3;
+                    }
+                    if (kim == 2)
+                    {
+                        H_yön = 3;
+                    }
+
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int geçiciX = karakterKonumuX;
+            int geçiciY = karakterKonumuY;
+
+            if (YenenYem == 298)
+            {
+
+                KırmızıHayalet.Stop();
+                timer1.Stop();
+                MessageBox.Show("KAZANDIN", "KAZANDIN");
+            }
+
+            if (karakterKonumuX == KırmızıHayaletX && karakterKonumuY == KırmızıHayaletY)
+            {
+                hak--;
+                label1.Text = hak.ToString();
+
+                karakterKonumuX = 14;
+                karakterKonumuY = 0;
+
+                KırmızıHayaletX = 14;
+                KırmızıHayaletY = 10;
+
+
+                if (hak == 0)
+                {
+
+                    KırmızıHayalet.Stop();
+                    timer1.Stop();
+                    MessageBox.Show("KAYBETTİN", "KAYBETTİN");
+                }
+                Thread.Sleep(1000);
+            }
+
+            if (İstenenYön != yön)
+            {
+                İstenenYönKontrol(karakterKonumuX, karakterKonumuY, İstenenYön, 1);
+            }
+
+            if (yön == 1)
+            {
+                if (geçiciX++ < 25 && yemler[karakterKonumuY, geçiciX].BackColor != Color.Transparent)
+                {
+                    if (yemler[karakterKonumuY, karakterKonumuX].BackColor == Color.Yellow)
+                    {
+                        YenenYem++;
+                        yemler[karakterKonumuY, karakterKonumuX].BackColor = Color.Black;
+                    }
+
+                    karakterKonumuX++;
+                    pictureBox1.Location = PAC_MAN(karakterKonumuY, karakterKonumuX);
+
+                }
+            }
+            else if (yön == 2)
+            {
+
+                if (geçiciX-- > 0 && yemler[karakterKonumuY, geçiciX].BackColor != Color.Transparent)
+                {
+                    if (yemler[karakterKonumuY, karakterKonumuX].BackColor == Color.Yellow)
+                    {
+                        YenenYem++;
+                        yemler[karakterKonumuY, karakterKonumuX].BackColor = Color.Black;
+                    }
+
+                    karakterKonumuX--;
+                    pictureBox1.Location = PAC_MAN(karakterKonumuY, karakterKonumuX);
+                }
+            }
+            else if (yön == 4)
+            {
+                if (geçiciY++ < 28 && yemler[geçiciY, karakterKonumuX].BackColor != Color.Transparent)
+                {
+                    if (yemler[karakterKonumuY, karakterKonumuX].BackColor == Color.Yellow)
+                    {
+                        YenenYem++;
+                        yemler[karakterKonumuY, karakterKonumuX].BackColor = Color.Black;
+                    }
+                    karakterKonumuY++;
+                    pictureBox1.Location = PAC_MAN(karakterKonumuY, karakterKonumuX);
+                }
+            }
+            else if (yön == 3)
+            {
+                if (geçiciY-- > 0 && yemler[geçiciY, karakterKonumuX].BackColor != Color.Transparent)
+                {
+                    if (yemler[karakterKonumuY, karakterKonumuX].BackColor == Color.Yellow)
+                    {
+                        YenenYem++;
+                        yemler[karakterKonumuY, karakterKonumuX].BackColor = Color.Black;
+                    }
+                    karakterKonumuY--;
+                    pictureBox1.Location = PAC_MAN(karakterKonumuY, karakterKonumuX);
+                }
+            }
+
+
+            label3.Text = YenenYem.ToString();
+            
+        }
+
+        //---------------------------------- karakter ---------------------------------------------
+
+        //---------------------------------- Hayalet ---------------------------------------------
+
+        byte yönBul(int x, int y)
+        {
+            int Y = KırmızıHayaletY;
+            int X = KırmızıHayaletX;
+
+            List<byte> liste = new List<byte>();
+
+            if (X++ < 25 && yemler[KırmızıHayaletY, X].BackColor != Color.Transparent)
+            {
+                liste.Add(1);
+            }
+            X = KırmızıHayaletX;
+            if (X-- > 0 && yemler[KırmızıHayaletY, X].BackColor != Color.Transparent)
+            {
+                liste.Add(2);
+            }
+            X = KırmızıHayaletX;
+            if (Y++ < 28 && yemler[Y, KırmızıHayaletX].BackColor != Color.Transparent)
+            {
+                liste.Add(4);
+            }
+            Y = KırmızıHayaletY;
+            if (Y-- > 0 && yemler[Y, KırmızıHayaletX].BackColor != Color.Transparent)
+            {
+                liste.Add(3);
+            }
+            int t = liste.Count;
+            int b = rasgele.Next(0, t);
+            byte a = liste[b];
+            return a;
+        }
+        byte etrafıKontrolEt(int x, int y)
+        {
+            int Y = KırmızıHayaletY;
+            int X = KırmızıHayaletX;
+
+            List<byte> liste = new List<byte>();
+
+            if (X++ < 25 && yemler[KırmızıHayaletY, X].BackColor != Color.Transparent)
+            {
+                liste.Add(1);
+            }
+            X = KırmızıHayaletX;
+            if (X-- > 0 && yemler[KırmızıHayaletY, X].BackColor != Color.Transparent)
+            {
+                liste.Add(2);
+            }
+            X = KırmızıHayaletX;
+            if (Y++ < 28 && yemler[Y, KırmızıHayaletX].BackColor != Color.Transparent)
+            {
+                liste.Add(4);
+            }
+            Y = KırmızıHayaletY;
+            if (Y-- > 0 && yemler[Y, KırmızıHayaletX].BackColor != Color.Transparent)
+            {
+                liste.Add(3);
+            }
+            byte t = (byte)liste.Count;
+            return t;
+        }
+        private void KırmızıHayalet_Tick(object sender, EventArgs e)
+        {
+            int geçiciX = KırmızıHayaletX;
+            int geçiciY = KırmızıHayaletY;
+            byte a;
+
+            if (karakterKonumuX == KırmızıHayaletX && karakterKonumuY == KırmızıHayaletY)
+            {
+
+                KırmızıHayalet.Stop();
+                timer1.Stop();
+
+                hak--;
+                label1.Text = hak.ToString();
+
+                karakterKonumuX = 14;
+                karakterKonumuY = 0;
+
+                KırmızıHayaletX = 14;
+                KırmızıHayaletY = 10;
+
+
+                KırmızıHayalet.Start();
+                timer1.Start();
+                if (hak == 0)
+                {
+                    KırmızıHayalet.Stop();
+                    timer1.Stop();
+                    MessageBox.Show("KAYBETTİN", "KAYBETTİN");
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            if (H_İstenenYön != H_yön)
+            {
+                İstenenYönKontrol(KırmızıHayaletX, KırmızıHayaletY, H_İstenenYön, 2);
+            }
+
+            // h_yön yönüne hareket etmesini sağlar karşısında duvar varsa durur
+            if (H_yön == 1)
+            {
+
+                if (geçiciX++ < 25 && yemler[KırmızıHayaletY, geçiciX].BackColor != Color.Transparent)
+                {
+                    a = etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY);
+
+                    KırmızıHayaletX++;
+                    pictureBox2.Location = PAC_MAN(KırmızıHayaletY, KırmızıHayaletX);
+
+                    if (etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY) > a)
+                    {
+                        H_İstenenYön = yönBul(karakterKonumuX, karakterKonumuY);
+                    }
+                }
+                else
+                {
+                    H_İstenenYön = yönBul(geçiciX--, geçiciY);
+                }
+            }
+            else if (H_yön == 2)
+            {
+
+                if (geçiciX-- > 0 && yemler[KırmızıHayaletY, geçiciX].BackColor != Color.Transparent)
+                {
+
+                    a = etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY);
+
+                    KırmızıHayaletX--;
+                    pictureBox2.Location = PAC_MAN(KırmızıHayaletY, KırmızıHayaletX);
+
+
+                    if (etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY) > a)
+                    {
+                        H_İstenenYön = yönBul(karakterKonumuX, karakterKonumuY);
+                    }
+                }
+                else
+                {
+                    H_İstenenYön = yönBul(geçiciX++, geçiciY);
+                }
+            }
+            else if (H_yön == 4)
+            {
+                if (geçiciY++ < 28 && yemler[geçiciY, KırmızıHayaletX].BackColor != Color.Transparent)
+                {
+                    a = etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY);
+
+                    KırmızıHayaletY++;
+                    pictureBox2.Location = PAC_MAN(KırmızıHayaletY, KırmızıHayaletX);
+
+
+                    if (etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY) > a)
+                    {
+                        H_İstenenYön = yönBul(karakterKonumuX, karakterKonumuY);
+                    }
+                }
+                else
+                {
+                    H_İstenenYön = yönBul(geçiciX, geçiciY--);
+                }
+            }
+            else if (H_yön == 3)
+            {
+                if (geçiciY-- > 0 && yemler[geçiciY, KırmızıHayaletX].BackColor != Color.Transparent)
+                {
+                    a = etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY);
+
+                    KırmızıHayaletY--;
+                    pictureBox2.Location = PAC_MAN(KırmızıHayaletY, KırmızıHayaletX);
+
+
+                    if (etrafıKontrolEt(KırmızıHayaletX, karakterKonumuY) > a)
+                    {
+                        H_İstenenYön = yönBul(karakterKonumuX, karakterKonumuY);
+                    }
+                }
+                else
+                {
+                    H_İstenenYön = yönBul(geçiciX, geçiciY++);
+                }
+            }
+
+        }
+
+
+        //---------------------------------- Hayalet ---------------------------------------------
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YemYerleştir();
+            BütünYemleriSil();
+            label1.Text = "3";
+            label2.Text = "Kalan Hak:";
+            pictureBox1.Location = PAC_MAN(karakterKonumuY, karakterKonumuX);
+            pictureBox2.Location = PAC_MAN(KırmızıHayaletY, KırmızıHayaletX);
+            KırmızıHayalet.Start();
+            this.KeyPreview = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            timer1.Start();
+            if (e.KeyCode == Keys.D)
+                İstenenYön = 1;
+            if (e.KeyCode == Keys.A)
+                İstenenYön = 2;
+            if (e.KeyCode == Keys.W)
+                İstenenYön = 3;
+            if (e.KeyCode == Keys.S)
+                İstenenYön = 4;
+
+        }
+
+    }
+}
